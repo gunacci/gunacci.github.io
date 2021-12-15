9dd8dc01e358e4287334e82591308067ebf8122d
gunacci <51497674+gunacci@users.noreply.github.com>
2021-12-15 01:43:59 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -20,23 +20,15 @@ window.onclick = function(event) {
   }
 }
 
-
-// modalni div
-
-let sveSlike = document.querySelectorAll("img");
+if ( document.URL.includes("index.html")||document.URL.includes("oNama.html")||document.URL.includes("autor.html") ) {
+  let sveSlike = document.querySelectorAll("img");
 
 for(let i = 0; i < sveSlike.length; i++){
   sveSlike[i].addEventListener("click", function() {
          document.getElementById("modalniRoditelj").style.display="flex";
         document.getElementById("modalniKontejner").innerHTML=`<img src="${sveSlike[i].src}" alt="${sveSlike[i].alt}"/ id="modalnaSlika">`+`<p id="modalniTekst"></p>`;
         document.getElementById("modalniKontejner").style.display="block";
-        /*if( sveSlike[i].hasAttribute("alt"))
-        {
-          document.getElementById("modalniTekst").innerHTML=`${sveSlike[i].alt}`;
-          document.getElementById("modalniTekst").id="modalniStil";
-          document.getElementById("modalniTekst").style.display="inline-block";
-        }
-        else;*/
+       
   });
 }
 
@@ -45,25 +37,44 @@ for(let i = 0; i < sveSlike.length; i++){
     document.getElementById("modalniKontejner").style.display="none";
   });
 
+  document.getElementById("modalniKontejner").addEventListener("click", function() {
+    document.getElementById("modalniRoditelj").style.display="none";
+    document.getElementById("modalniKontejner").style.display="none";
+  });
+}
 
-  let prodavnicaSlike = document.querySelectorAll("slikaArtikal");
-  for(let i = 0; i < prodavnicaSlike.length; i++){
-    sveSlike[i].addEventListener("click", function() {
-           document.getElementById("modalniRoditelj2").style.display="block";
-          document.getElementById("modalniKontejner2").innerHTML=`<img src="${prodavnicaSlike[i].img.src}" alt="${prodavnicaSlike[i].img.alt}"/ id="modalnaSlika">`+`<p id="modalniTekst"></p>`;
-          document.getElementById("modalniKontejner2").style.display="block";
-          if( sveSlike[i].hasAttribute("alt"))
-          {
-            document.getElementById("modalniTekst").innerHTML=`${prodavnicaSlike[i][i].p.innerHTML}`;
-            document.getElementById("modalniTekst").id="modalniStil";
-            document.getElementById("modalniTekst").style.display="inline-block";
-          }
-          else;
+// modalni div prodavnica
+
+else if ( document.URL.includes("prodavnica.html") ) {
+
+  let prodavnicaSlike = main.querySelectorAll("img");
+  let prodavnicaTekstNaslovi= main.querySelectorAll("h4");
+  let prodavnicaOpisi=main.querySelectorAll("p");
+  let prodavnicaCene=main.querySelectorAll(".cena");
+  for(let i = 0; i < prodavnicaSlike .length; i++){
+    prodavnicaSlike [i].addEventListener("click", function() {
+           document.getElementById("modalniRoditelj").style.display="flex";
+          document.getElementById("modalniKontejner2").innerHTML=`<div><img src="${prodavnicaSlike[i].src}" alt="${prodavnicaSlike[i].alt}" id="modalnaSlika2"/></div>`+`<article class="jednostavanPasus">
+          <h4>${prodavnicaTekstNaslovi[i].innerHTML}</h4>
+          <p> ${prodavnicaOpisi[i].innerHTML}
+          </p>
+              <span class="cena">${prodavnicaCene[i].innerHTML}</span></a>
+              <span class="korpa"><i class="fa fa-shopping-cart"></i></span>
+      </article>`;
+          document.getElementById("modalniKontejner2").style.display="flex";
+          document.getElementById("modalniRoditelj").addEventListener("click", function() {
+            document.getElementById("modalniRoditelj").style.display="none";
+            document.getElementById("modalniKontejner2").style.display="none";
     });
   }
 
-  document.getElementById("modalniRoditelj2").addEventListener("click", function() {
-    document.getElementById("modalniRoditelj2").style.display="none";
-    document.getElementById("modalniKontejner2").style.display="none";
-    document.getElementById("modalniTekst").style.display="none";
-  });
+
+  
+
+
+  
+    
+  );
+  
+}
+}
